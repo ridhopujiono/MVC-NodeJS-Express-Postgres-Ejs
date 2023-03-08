5862d050fce30e0d0db4098a36f6c7bf1945cf18
Ridho Pujiono <ridho@smktjp.sch.id>
2023-03-08 15:27:59 +0700

    berhasil koneksi ke DB

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres'
+});
+// Menguji koneksi ke database
+db
+    .authenticate()
+    .then(() => {
+        console.log('Koneksi berhasil.');
+    })
+    .catch((err) => {
+        console.error('Koneksi gagal:', err);
+    });
+
+module.exports = db;
